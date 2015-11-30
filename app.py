9e8c1f1ac9e242fce1679433ced6bfbb7095a5fc
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2015-11-30 17:58:35 +0200

    feat(app): Working deque to process incomming frames

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response, request, jsonify
 from video_deque import VideoDeque
+from camera_cv2 import VideoCamera
 import logging
 import datetime
 import time
+
 # import unicode
 
 app = Flask(__name__)
@@ -13,14 +15,22 @@ app.logger.setLevel(logging.INFO)  # set the desired logging level here
 app.logger.info('app started')
 
 video = None
+cv_video = None
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/00.jpg')
+def test_img():
+    return open('00.jpg', 'rb').read()
 
 def gen(camera):
     while True:
         frame = camera.deque_frame()
+        if frame == None: 
+            print('Nothing to deque')
+            break
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -29,16 +39,20 @@ def video_feed():
     return Response(gen(video),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/upload_frame',methods=['POST'],)
+@app.route('/cv_feed',methods=['GET'])
+def cv_feed():
+    return Response(gen(cv_video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/upload_frame',methods=['POST'])
 def upload_frame():
     start = datetime.datetime.now()
-    jsonBody = request.get_json(silent=True)
-    content = jsonBody.get('content')
-    video.enque_frame(content)
-    # return json.dumps(result)
-    # retval = jsonify({'results': {'caras': caras}})
+    frame = request.files.get('frame')
+    cv_video.enque_frame(frame)
     return jsonify({'results': 'enque_frame success'})
 
 if __name__ == '__main__':
-    video = VideoDeque()
-    app.run(host='0.0.0.0', debug=True)
+    video = VideoCamera()
+    cv_video = VideoDeque()
+    app.run(host='0.0.0.0', debug=True, threaded=True)
+    video.stop()
--- video_deque.py
+++ video_deque.py
@@ -1,47 +1,46 @@
-# camera_cv2.py
+# video_deque.py
 from PIL import Image
+from fs.memoryfs import MemoryFS
+import fs.path
+import PIL
 import io
 import numpy as np
 import cv2
 import collections
-import base64
-# import unicodedata
+import datetime
+import tempfile
+import string
+import random
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 class VideoDeque(object):
     def __init__(self):
         self.frame_deque = collections.deque(maxlen=100)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
-        self.cat_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv3/3.0.0/share/OpenCV/haarcascades/haarcascade_frontalcatface.xml')
         self.face_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv3/3.0.0/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
         self.eye_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv3/3.0.0/share/OpenCV/haarcascades/haarcascade_eye.xml')
     
     def enque_frame(self, frame):
-        # if not isinstance(frame, unicodedata):
-        #     return []
-        # image_data = re.sub('^data:image/.+;base64,', '', frame).decode('base64')
-        image_data = base64.b64decode(frame)
-        image = Image.open(io.BytesIO(image_data))
-        # image = Image.open(io.StringIO(frame))
-        image = cv2.cvtColor(np.array(image), 2)
-        self.frame_deque.append(frame)
+        frame_io = io.BytesIO()
+        frame.save(frame_io)
+        self.frame_deque.append(frame_io)
+        print('new length is '+str(len(self.frame_deque)))
 
     
     def deque_frame(self):
+        print('deque length is '+str(len(self.frame_deque)))
         if len(self.frame_deque) <= 0:
-             return b''
-        image = self.frame_deque.popleft()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
+             return None
+        print('dequeing image')
+        frame_io = self.frame_deque.popleft()
+        nparr = np.array(frame_io.getbuffer(), np.ubyte)
+        image = cv2.imdecode(nparr,1)
+        print(image.size)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # if self.cat_cascade.empty() != True:
-        #     cats = self.cat_cascade.detectMultiScale(image, 1.3, 5)
-        #     for (x,y,w,h) in cats:
-        #         cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2)
         if self.face_cascade.empty() != True:
             faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+            print('faces length is '+str(len(self.frame_deque)))
             for (x,y,w,h) in faces:
                 cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
                 if self.face_cascade.empty() != True:
@@ -50,7 +49,11 @@ class VideoDeque(object):
                     eyes = self.eye_cascade.detectMultiScale(roi_gray)
                     for (ex,ey,ew,eh) in eyes:
                         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        ret, jpeg = cv2.imencode('.jpg', image)
+        else:
+            print('face cascade not open')
+        imagergb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        ret, jpeg = cv2.imencode('.jpg', imagergb)
         if ret == True:
             return jpeg.tobytes()
+        print('cv2 failed')
         return b''
